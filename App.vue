28c61b1196379d7ee171d37137ae67dc5d18be1a
Denilson gomes da Conceicao <121492709+Danin07@users.noreply.github.com>
2023-06-23 02:44:46 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<script>
+import Header from './components/Header.vue'
+import Container from './components/Container.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Container
+  }
+}
+
+</script>
+
+<template>
+
+  <Header/>
+
+</template>
+
+
+
+<style scoped>
+
+p{
+  background: red;
+  color: blue;
+}
+
+
+
+</style>
